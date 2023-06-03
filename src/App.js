e2da3f79b6ca692d364b75634aa255279128e11b
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-02 22:48:45 -0500

    Connect Provider Metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from './views/home';
 
 function App() {
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({
+          method: 'eth_requestAccounts'
+        })
+        .then(console.log);
+    } else {
+      console.log("CLiente no tiene un provider");
+    }
+
+
+
+  }, []);
   return (
     <>
       <Routes>
