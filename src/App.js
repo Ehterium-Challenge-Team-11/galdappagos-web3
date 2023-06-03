38832dd052b6c581f69432788940b9b3a25d56e1
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-02 23:05:18 -0500

    Conect Wallet with lib web3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from './views/home';
+// import Web3 from "web3";
+import Web3 from "web3/dist/web3.min";
 
 function App() {
 
   useEffect(() => {
     if (window.ethereum) {
+      /*
       window.ethereum
         .request({
           method: 'eth_requestAccounts'
         })
         .then(console.log);
+      */
+
+      console.log("Galdappagos");
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+
     } else {
-      console.log("CLiente no tiene un provider");
+      console.log("Cliente no tiene un provider");
     }
-
-
-
   }, []);
   return (
     <>
