f69f44cc807569ba5ca5ca9fbde8b2548ed3989d
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-03 23:25:39 -0500

    Implement Web3Provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Web3ReactProvider } from "@web3-react/core";
+import { getLibrary } from "./config/web3";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode><HashRouter>
     <ChakraProvider>
-      <HashRouter>
+      <Web3ReactProvider getLibrary={getLibrary}>
         <App />
-      </HashRouter>
+      </Web3ReactProvider>
     </ChakraProvider>
+  </HashRouter>
+
   </React.StrictMode>,
 );
