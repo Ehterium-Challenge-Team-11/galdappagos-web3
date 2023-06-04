c5a9715dda7afea3ee7c3ca62e751fc9e825d31e
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-03 23:54:13 -0500

    Layout Main - Footer

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from './views/home';
 // import Web3 from "web3";
-import Web3 from "web3/dist/web3.min";
+// import Web3 from "web3/dist/web3.min";
 
 function App() {
 
   useEffect(() => {
+    /*
+    // 
     if (window.ethereum) {
-      /*
-      window.ethereum
-        .request({
-          method: 'eth_requestAccounts'
-        })
-        .then(console.log);
-      */
-
-      console.log("Galdappagos");
       const web3 = new Web3(window.ethereum);
       web3.eth.requestAccounts().then(console.log);
 
     } else {
       console.log("Cliente no tiene un provider");
-    }
+    }*/
   }, []);
   return (
     <>
--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,4 +1,5 @@
-import Web3 from "web3";
+//import Web3 from "web3";
+import Web3 from "web3/dist/web3.min";
 import { InjectedConnector } from "@web3-react/injected-connector";
 
 
