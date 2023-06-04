c85eeedf321767f639cb86489a1726f70c62bf91
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-03 23:50:13 -0500

    Config packages version 6.0 web3-react

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,6 +1,14 @@
 import Web3 from "web3";
 import { InjectedConnector } from "@web3-react/injected-connector";
 
+
+/*
+import { initializeConnector } from '@web3-react/core'
+import { MetaMask } from '@web3-react/metamask'
+export const [metaMask, hooks] = initializeConnector<MetaMask>((actions) => new MetaMask({ actions })) */
+
+
+
 const connector = new InjectedConnector({
     supportedChainIds: [
         11155111, // Sepolia
@@ -12,3 +20,4 @@ const connector = new InjectedConnector({
   };
   
   export { connector, getLibrary };
+  
